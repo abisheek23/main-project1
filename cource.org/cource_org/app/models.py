a3418f31b1334f8accb0d9a688c408bce8ab2e82
abisheek23 <abisheek2397@gmail.com>
2024-11-25 11:31:12 +0530

    e_com

--- cource.org/cource_org/app/models.py
+++ cource.org/cource_org/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class cource(models.Model):
+    cid=models.TextField()
+    cource_name=models.TextField()
+    cource_dis=models.TextField()
+    price=models.IntegerField()
+    offer_price=models.IntegerField()
+    img=models.FileField()
--- cource.org/cource_org/app/views.py
+++ cource.org/cource_org/app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from .models import *
 
 # Create your views here.
+def home(req):
+
+    #    data=cource.objects.all()
+        return render(req,'index.html')
+ 
